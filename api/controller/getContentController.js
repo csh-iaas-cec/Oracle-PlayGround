9ca740fcb90e8eb3309822335591b651a3e1712a
mrm <raghib@ignitist.co.in>
2019-06-25 16:04:46 +0530

    git api's

--- api/controller/getContentController.js
+++ api/controller/getContentController.js
@@ -2,7 +2,9 @@
 const GetContentService = require('../services/getContent.service');
 
 exports.getContent = function(req,res,next){
+    
     var getContent = new GetContentService();
+    
     getContent.getContentCall(req,function(err,result){
         if(err){
             return next(err);
@@ -14,6 +16,7 @@ exports.getContent = function(req,res,next){
 }
 
 exports.putContent = function(req,res,next){
+    // console.log("===request in controller ====",req);
     var putContent = new GetContentService();
     putContent.putContentCall(req,function(err,result){
         if(err){
--- api/services/getContent.service.js
+++ api/services/getContent.service.js
@@ -4,7 +4,7 @@ var request = require("request");
 class GetContentService{
 
     getContentCall(req,callback){
-
+     
        
         var options = { method: 'GET',
           url: 'https://api.github.com/repos/csh-iaas-cec/Oracle-PlayGround/contents/var.tf',
@@ -15,7 +15,7 @@ class GetContentService{
              'Cache-Control': 'no-cache',
              Accept: '*/*',
              'User-Agent': 'PostmanRuntime/7.15.0',
-             //add authorize token
+             //auth to be added
              'Content-Type': 'application/json' } };
         
         request(options, function (error, response, body) {
@@ -27,27 +27,26 @@ class GetContentService{
     }
 
     putContentCall(req,callback){
-        console.log("=== request for upload===",req);
-
+        // console.log("=== request for upload===",req);
+        // let data = req.body;  
+        // let buff = new Buffer.alloc(data);  
+        // let base64data = buff.toString('base64');
         var options = { method: 'PUT',
   url: 'https://api.github.com/repos/csh-iaas-cec/Oracle-PlayGround/contents/var.tf',
   headers: 
    { 'cache-control': 'no-cache',
      Connection: 'keep-alive',
-     'content-length': '211',
-     'accept-encoding': 'gzip, deflate',
      Host: 'api.github.com',
-     'Postman-Token': 'f79debc7-da4a-41d3-96fc-b823d02e2681,b7ebb1d2-dc87-4dbd-854e-8b4a579f2c87',
      'Cache-Control': 'no-cache',
      Accept: '*/*',
      'User-Agent': 'PostmanRuntime/7.15.0',
-     //add authorize token
+     //auth to be added
      'Content-Type': 'application/json' },
   body: 
    { message: 'my commit message',
      committer: { name: 'csh-iaas-cec', email: 'ravi.devarakonda@oracle.com' },
-     content: req.body,
-     sha: 'e4d3ec48b1faa2789c9db6c443e9774beb26d240' },
+     content: "dmFyIHJlZ2lvbiB7CiAgICAgZGVmYXVsdCA6ICJ1cy1hc2hidXJuLTEiCn0=",
+     sha: '61cfcf04218e50585e377c3349814bf79322455e' },
   json: true };
 
 request(options, function (error, response, body) {
