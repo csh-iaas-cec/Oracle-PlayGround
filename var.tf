0d1f6261bf351d34f3dfcf39391f37002886fe7e
csh-iaas-cec <51996734+csh-iaas-cec@users.noreply.github.com>
2019-08-13 20:30:09 +0530

    Update var.tf

--- var.tf
+++ var.tf
@@ -1,1 +1,12 @@
-
+variable "region" {
+                        default = "Ashburn"  
+                      }
+                      variable "fn"{
+                        default = "Kishore"
+                      }
+                      variable "ln"{
+                        default = "Kuar"
+                      }
+                      variable "email"{
+                        default = "kis@gmail"
+                      }
